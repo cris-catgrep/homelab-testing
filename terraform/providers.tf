e523f3eca7bef82592d4b56837f99f1614f5f0dd
PenPen <you@example.com>
2026-05-11 20:03:20 -0600

    Se corrigieron errores

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,9 +2,9 @@ provider "proxmox" {
   endpoint = "https://10.0.0.2:8006/"
 
   # TODO: use terraform variable or remove the line, and use PROXMOX_VE_USERNAME environment variable
-  username = "root@pam"
+  username = var.proxmox-user
   # TODO: use terraform variable or remove the line, and use PROXMOX_VE_PASSWORD environment variable
-  password = "the-password-set-during-installation-of-proxmox-ve"
+  password = var.proxmox-password
 
   # because self-signed TLS certificate is in use
   insecure = true
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,11 @@
 variable "proxmox-user" {
     type = string
     description = "Usuario de Proxmox"
-    default = ""
     sensitive = true
 }
 
 variable "proxmox-password" {
     type = string
     description = "Password para el usuario de Proxmox"
-    default = ""
     sensitive = true
 }
